2647b7ad6148956c9f8408aa2c54a2652c680113
Will Hains <willhains@users.noreply.github.com>
2020-01-26 22:44:27 +0900

    Use custom consumers.

--- src/com/willhains/sum4j/Sum.java
+++ src/com/willhains/sum4j/Sum.java
@@ -1,8 +1,6 @@
 package com.willhains.sum4j;
 
-import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
-import java.util.function.Consumer;
 
 public abstract class Sum<This extends Sum<This>>
 {
@@ -15,19 +13,19 @@ public abstract class Sum<This extends Sum<This>>
 		_values = values;
 	}
 	
-	public final void ifCase(final Case0<This> case0, final Runnable then)
+	public final void ifCase(final Case0<This> case0, final Case0.Then then)
 	{
-		if(case0 == _case) then.run();
+		if(case0 == _case) then.handle();
 	}
 	
-	public final <A> void ifCase(final Case1<This, A> case1, final Consumer<A> then)
+	public final <A> void ifCase(final Case1<This, A> case1, final Case1.Then<A> then)
 	{
-		if(case1 == _case) then.accept((A)_values[0]);
+		if(case1 == _case) then.handle((A)_values[0]);
 	}
 	
-	public final <A, B> void ifCase(final Case2<This, A, B> case2, final BiConsumer<A, B> then)
+	public final <A, B> void ifCase(final Case2<This, A, B> case2, final Case2.Then<A, B> then)
 	{
-		if(case2 == _case) then.accept((A)_values[0], (B)_values[1]);
+		if(case2 == _case) then.handle((A)_values[0], (B)_values[1]);
 	}
 	
 	protected @FunctionalInterface interface Case<SumType extends Sum<SumType>>
@@ -40,11 +38,20 @@ public abstract class Sum<This extends Sum<This>>
 	}
 	
 	public @FunctionalInterface interface Case0<SumType extends Sum<SumType>> extends Case<SumType>
-	{ default SumType init() { return construct(); } }
+	{
+		default SumType init() { return construct(); }
+		static @FunctionalInterface interface Then { void handle(); }
+	}
 	
 	public @FunctionalInterface interface Case1<SumType extends Sum<SumType>, A> extends Case<SumType>
-	{ default SumType init(A a) { return construct(a); } }
+	{
+		default SumType init(A a) { return construct(a); }
+		static @FunctionalInterface interface Then<A> { void handle(A a); }
+	}
 	
 	public @FunctionalInterface interface Case2<SumType extends Sum<SumType>, A, B> extends Case<SumType>
-	{ default SumType init(A a, B b) { return construct(a, b); } }
+	{
+		default SumType init(A a, B b) { return construct(a, b); }
+		static @FunctionalInterface interface Then<A, B> { void handle(A a, B b); }
+	}
 }
