00a22a48c789a78b698544e4636acc4716ec0c0f
Will Hains <will@willhains.com>
2020-04-27 13:00:44 +0900

    Add example code to demo toString() and equals().

--- examples/example1/Example1.java
+++ examples/example1/Example1.java
@@ -6,22 +6,35 @@ public class Example1
 {
 	public static void main(final String[] args)
 	{
-		final Shape shape = Shape.RECTANGLE.init(3.0, 4.5);
+		final Shape rect1 = Shape.RECTANGLE.init(3.0, 4.5);
+		final Shape rect2 = Shape.RECTANGLE.init(3.0, 4.5);
+		final Shape rect3 = Shape.RECTANGLE.init(1.0, 2.0);
+		final Shape circ1 = Shape.CIRCLE.init(3.0);
+		final Shape point = Shape.POINT.init();
 		
-		shape.ifCase(Shape.CIRCLE, radius -> drawCircle(radius));
+		circ1.ifCase(Shape.CIRCLE, radius -> drawCircle(radius));
 		
-		final double area1 = shape
+		final double area1 = rect1
 			.mapCase(Shape.RECTANGLE, (width, height) -> width * height)
 			.mapCase(Shape.CIRCLE,    (radius)        -> Math.PI * radius * radius)
 			.orElse(0.0);
 		System.out.println("area1 = " + area1);
 		
-		final double area2 = shape
-			.mapCase(Shape.RECTANGLE, (width, height) -> width * height)
-			.mapCase(Shape.CIRCLE,    (radius)        -> Math.PI * radius * radius)
+		final double area2 = circ1
 			.mapCase(Shape.POINT,     ()              -> 0.0)
+			.mapCase(Shape.CIRCLE,    (radius)        -> Math.PI * radius * radius)
+			.mapCase(Shape.RECTANGLE, (width, height) -> width * height)
 			.orElseThrow();
 		System.out.println("area2 = " + area2);
+		
+		System.out.println(Shape.RECTANGLE.init(3.0, 4.5));
+		System.out.println(Shape.CIRCLE.init(5.1));
+		System.out.println(Shape.POINT.init());
+		
+		System.out.println(rect1 + " " + (rect1.equals(rect1)?"==":"!=") + " " + rect1);
+		System.out.println(rect1 + " " + (rect1.equals(rect2)?"==":"!=") + " " + rect2);
+		System.out.println(rect1 + " " + (rect1.equals(rect3)?"==":"!=") + " " + rect3);
+		System.out.println(rect1 + " " + (rect1.equals(circ1)?"==":"!=") + " " + circ1);
 	}
 	
 	static void drawCircle(final double radius)
